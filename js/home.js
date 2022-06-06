df2b2c9b757bf6038b20b8cba6daf4824bca8b77
ShivamSali <shivamsali81097@gmail.com>
2022-06-06 17:24:09 +0530

    done

--- js/home.js
+++ js/home.js
@@ -14,3 +14,16 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+    window.onload = function(){
+        setTimeout(function(){
+            popup.style.display = "block"
+
+            // Add some time delay to show popup
+        }, 2000)
+    }
+    close.addEventListener('click', () => {
+        popup.style.display = "none";
+    })
